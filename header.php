250b1d9de0d6162f7492705b2c4b9999a343e86e
Xrimm <hopeof1percent@gmail.com>
2025-12-28 02:16:03 +0800

    Membuat : - header untuk web beserta link ke halaman yang akan ada

---
+++ header.php
@@ -0,0 +1,13 @@
+<header id="mainHeader">
+    <div class="logo">
+        <img src="images/logo.png" alt="Casholle Logo">
+    </div>
+
+    <nav>
+        <a href="index.php">News</a>
+        <a href="#">FreeChats</a>
+        <a href="#">Shop</a>
+        <a href="#">About Us</a>
+        <a href="#">Profile</a>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include "data.php"; ?>
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <title>Casholle</title>
+        <link rel="stylesheet" href="css_assets/style.css">
+        <link rel="stylesheet" href="css_assets/header.css">
+    </head>
+    
+    <body>
+
+        <?php include 'header.php'; ?>
+
+    </body>
+</html>
