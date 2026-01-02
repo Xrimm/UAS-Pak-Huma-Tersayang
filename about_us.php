972a41a8d60399d7a751404787221ca65f4eb41d
danielciboy <danielndolu374@gmail.com>
2026-01-02 17:28:58 +0800

    membuat file dan halaman untuk about us

---
+++ about_us.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us - Casholle</title>
+
+    <link rel="stylesheet" href="css_assets/header.css">
+    <link rel="stylesheet" href="css_assets/style.css">
+    <link rel="stylesheet" href="css_assets/about_us.css">
+</head>
+<body>
+
+<?php include 'header.php'; ?>
+
+<div class="container">
+    <main>
+        <h2>About Casholle</h2>
+        <p class="tagline">Collect, Trade, and Play with Passion</p>
+
+        <section>
+            <h3>Who We Are</h3>
+            <p class="about-paragaraf1">
+                Casholle adalah platform penjualan dan koleksi kartu permainan yang
+                didedikasikan bagi para kolektor dan pemain kartu di Indonesia, dengan
+                tujuan menyediakan berbagai cardsets berkualitas tinggi yang dirancang
+                secara eksklusif dan diproduksi dalam edisi terbatas. Kami berkomitmen
+                untuk menghadirkan kartu permainan yang tidak hanya memiliki nilai
+                fungsional sebagai media bermain, tetapi juga nilai estetika dan koleksi
+                yang tinggi. Melalui desain yang inovatif, kualitas material yang
+                terjamin, serta proses seleksi produk yang ketat, Casholle berupaya
+                menciptakan pengalaman berbelanja yang aman, nyaman, dan terpercaya,
+                sekaligus mendukung pertumbuhan komunitas kolektor kartu yang kreatif,
+                solid, dan berkelanjutan di Indonesia.
+            </p>
+        </section>
+
+        <section>
+            <h4>Our Staff</h4>
+            <div class="dashboard">
+                <a href="#"><img alt="staff1"></a>
+                <a href="#"><img alt="staff1"></a>
+                <a href="#"><img alt="staff1"></a>
+                <a href="#"><img alt="staff1"></a>
+            </div>
+
+        </section>
+    </main>
+</div>
+
+</body>
+</html>
